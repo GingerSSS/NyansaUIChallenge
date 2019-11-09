0fc8eabb8a7dea8f16457f377e30d8a36c7482de
GingerS <57018222+GingerSSS@users.noreply.github.com>
2019-11-09 15:24:27 -0800

    Update topfive.js

--- interview-app-devmgr/src/app/common/mysort/topfive.js
+++ interview-app-devmgr/src/app/common/mysort/topfive.js
@@ -1,7 +1,9 @@
 // js sort object array by key -> https://stackoverflow.com/questions/8837454/sort-array-of-objects-by-single-key-with-date-value
 // javascript trim array -> https://stackoverflow.com/questions/953071/how-to-easily-truncate-an-array-with-javascript/6928247
+
 function mysort(input, key) {
-    return input.sort(function (a, b)
+    // sort an array not in place -> https://stackoverflow.com/questions/9592740/how-can-you-sort-an-array-without-mutating-the-original-array
+    return input.concat().sort(function (a, b)
     {
         var m = a[key];
         var n = b[key];
